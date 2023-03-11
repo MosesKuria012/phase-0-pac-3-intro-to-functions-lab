3d86923e5424f78c2b88cbb97bdc3b2690d04154
Moses <t.content012@gmail.com>
2023-03-11 14:44:22 +0300

    DIfficult

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+return string.toUpperCase()
+}
+shout()
+
+function whisper(string){
+return string.toLowerCase()
+}
+whisper()
+
+function logShout(string){
+console.log (string.toUpperCase())
+}
+logShout()
+
+function logWhisper(string){
+    console.log (string.toLowerCase())
+    }
+    logWhisper()
+
+    
+
+
+function sayHiToHeadphonedRoommate(string){
+    if (string==='hello')
+    return "I can\'t hear you!"
+    else if (string==='HELLO')
+    return "YES INDEED!"
+    else (string==="I would love to!")
+    return "I would love to!"
+}
+    
+sayHiToHeadphonedRoommate()
